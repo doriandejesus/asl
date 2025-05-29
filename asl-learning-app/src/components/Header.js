0fd9b571f0459210da668535ddc1ed9bcaff0391
Dorian De Jesus <dorian1dejesus@gmail.com>
2025-05-28 23:01:44 -0400

    Code base and imports

---
+++ asl-learning-app/src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header style={styles.header}>
+            <h1 style={styles.title}>ASL Learning App</h1>
+        </header>
+    );
+};
+
+export default Header;
---
+++ asl-learning-app/src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Welcome to the ASL Learning App</h1>
+            <p>Start your journey to learn American Sign Language today!</p>
+        </div>
+    );
+};
+
+export default Home;
