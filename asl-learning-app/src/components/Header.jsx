aae760ca4948b790e6a87130e95bd3f54d17173f
Dorian De Jesus <dorian1dejesus@gmail.com>
2025-11-14 01:30:17 -0500

    themes and colors

--- asl-learning-app/src/components/Header.jsx
+++ asl-learning-app/src/components/Header.jsx
@@ -1,20 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './Header.css';
 import { GoHomeFill } from "react-icons/go";
-
+import { loadSavedTheme } from "../theme";
 
 const Header = () => {
     const [menuOpen, setMenuOpen] = React.useState(false);
 
+    useEffect(() => {
+        loadSavedTheme();
+    }, []);
+
     return (
         <div className='header'>
             <div 
-            className='logo'
-            onClick={() => {
-                window.location.href = '/';
-                document.body.style.cursor = 'pointer';
-                setMenuOpen(false);
-            }}>
+                className='logo'
+                onClick={() => {
+                    window.location.href = '/';
+                    document.body.style.cursor = 'pointer';
+                    setMenuOpen(false);
+                }}
+            >
                 <h1 className='large'>ASL</h1>
                 <p className='small'>
                     American<br/>
@@ -28,7 +33,6 @@ const Header = () => {
                 onClick={() => setMenuOpen(!menuOpen)}
                 size={30}
             />
-
         </div>
     );
 };
